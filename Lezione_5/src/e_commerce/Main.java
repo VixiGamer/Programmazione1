819922ff5eb5824879e2e746cb3c623bb4069a32
VixiGamer <viggoponturonygaard@gmail.com>
2025-06-20 12:27:00 +0200

    aggiornamento esercizi

--- Lezione_5/src/e_commerce/Main.java
+++ Lezione_5/src/e_commerce/Main.java
@@ -3,11 +3,12 @@ package e_commerce;
 public class Main {
 
 	public static void main(String[] args) {
-		Carrello agiungiProdotti = new Carrello();
+		/*Carrello ag = new Carrello();*/
 		
 		System.out.println(Carrello.aggiungiProdotti("Mele"));
-		
 		System.out.println(Carrello.aggiungiProdotti("Pere", 4));
+		System.out.println(Carrello.aggiungiProdotti("Banane", 2));
+		System.out.println(Carrello.aggiungiProdotti("Pesche", 10));
 
 	}
 
